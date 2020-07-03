63f840cb24969281507d68eda34fb2096218e9e2
AbingPj <pj.abing@gmail.com>
2020-07-03 21:34:52 +0800

    Updated Welcome Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@ last commit
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>E-Pawn (VERSION 3)</title>
+  <title>E-Pawn</title>
      <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body class="hold-transition sidebar-mini">
