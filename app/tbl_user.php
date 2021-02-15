14cb78646100eaba0aa1ccf7cbc0e7ba35db85a8
AbingPj <pj.abing@gmail.com>
2021-02-16 00:31:04 +0800

    Added Image Link Attribute for the user

--- app/tbl_user.php
+++ app/tbl_user.php
@@ -30,4 +30,33 @@ class tbl_user extends Model
         return $this->hasMany('App\zPackage', 'pawnshop_id', 'user_id');
     }
 
+
+    protected $appends = [
+        'atr_image_link',
+    ];
+
+    public function getAtrImageLinkAttribute()
+    {
+
+        if (!empty($this->image)) {
+
+            // // explode by /
+            // $explode_path = explode('/', $this->image);
+            // // removed first value in array wich is the public of the path
+            // unset($explode_path[0]);
+            // // return back to his format
+            // $implode_path = implode('/', $explode_path);
+            // $photo = url('storage/' . $implode_path);
+
+            $photo = url('/images/'.$this->image);
+        } else {
+            // $photo = $this->is_bundle == 1 ? "/img/bundle.png" : "/img/no-product-image.png";
+            $photo = "";
+        }
+
+        return $photo;
+    }
+
+
+
 }
